08cad212cf8fd832281600585c510647d95ab90e
Maxim Malashchuk <maximopolnate@gmail.com>
2020-12-02 20:18:36 +0300

    Initial commit

---
+++ src/main/java/com/maximopol/maxtut/MaxtutApplication.java
@@ -0,0 +1,13 @@
+package com.maximopol.maxtut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxtutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxtutApplication.class, args);
+	}
+
+}
