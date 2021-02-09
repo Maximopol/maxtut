79c1a04e4363d60424414d0f8b89f9a256aa21cd
Maksim <maximopolnate@gmail.com>
2021-02-09 22:27:27 +0300

    transferring the project to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/maxtut-0.0.1-SNAPSHOT.war
--- src/main/java/com/maximopol/maxtut/MaxtutApplication.java
+++ src/main/java/com/maximopol/maxtut/MaxtutApplication.java
@@ -1,9 +1,13 @@
 package com.maximopol.maxtut;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableEncryptableProperties
+@PropertySource("classpath:database.properties")
 public class MaxtutApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MaxtutApplication.class, args);
