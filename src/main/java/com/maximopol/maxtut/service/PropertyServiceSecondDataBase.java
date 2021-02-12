aae2c60f1c3d587d99f28ba966f097e4f3280aab
Maksim <maximopolnate@gmail.com>
2021-02-12 20:12:48 +0300

    Change properties

--- src/main/java/com/maximopol/maxtut/service/PropertyServiceSecondDataBase.java
+++ src/main/java/com/maximopol/maxtut/service/PropertyServiceSecondDataBase.java
@@ -1,20 +1,10 @@
 package com.maximopol.maxtut.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PropertyServiceSecondDataBase {
-    private final Logger logger = LoggerFactory.getLogger(PropertyServiceSecondDataBase.class);
-
-    public PropertyServiceSecondDataBase() {
-        logger.info("I am cat");
-        logger.info(username);
-        logger.info(password);
-    }
-
     @Value("${questions.username}")
     private String username;
     @Value("${questions.password}")
