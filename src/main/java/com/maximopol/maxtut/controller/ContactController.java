5c112a34bb3d6c7c48db83ebe324256aa97571c9
Maksim <maximopolnate@gmail.com>
2021-02-13 23:23:29 +0300

    Change ContactController

--- src/main/java/com/maximopol/maxtut/controller/ContactController.java
+++ src/main/java/com/maximopol/maxtut/controller/ContactController.java
@@ -1,9 +1,12 @@
 package com.maximopol.maxtut.controller;
 
 import com.maximopol.maxtut.entity.User;
+import com.maximopol.maxtut.entitykek.Questions;
 import com.maximopol.maxtut.service.PropertyServiceSecondDataBase;
+import com.maximopol.maxtut.service.QuestionsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,10 +17,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 //import com.maximopol.maxtut.service.PropertyServiceSecondDataBase;
 
@@ -26,7 +25,8 @@ import java.sql.SQLException;
 @SessionAttributes(value="user")
 public class ContactController {
     private final Logger logger = LoggerFactory.getLogger(ContactController.class);
-
+    @Autowired
+    QuestionsService questionsService;
     final PropertyServiceSecondDataBase propertyServiceSecondDataBase;
 
     public ContactController(PropertyServiceSecondDataBase propertyServiceSecondDataBase) {
@@ -43,21 +43,24 @@ public class ContactController {
     public ModelAndView sentQuestionToMyEmail(HttpServletRequest req, HttpServletResponse res, Model model, @ModelAttribute("user") User user) {
         String email = req.getParameterValues("email")[0], text = req.getParameterValues("message")[0];
 
-
-       // String SQL = "INSERT INTO public.\"questions \" (email,text) VALUES(?,?)";
-        String SQL = "INSERT INTO `Questions` ( `Email`, `Text`) VALUES(?,?)";
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection dbConnection = DriverManager.getConnection(propertyServiceSecondDataBase.getUrl(), propertyServiceSecondDataBase.getUsername(), propertyServiceSecondDataBase.getPassword());
-            PreparedStatement statement = dbConnection.prepareStatement(SQL);
-            statement.setString(1, email);
-            statement.setString(2, text);
-            statement.addBatch();
-            statement.executeBatch();
-            System.out.println("123123");
-        } catch (SQLException | ClassNotFoundException e) {
-            logger.error(e.toString());
-        }
+        Questions questions= new Questions();
+        questions.setEmail(email);
+        questions.setText(text);
+        questionsService.saveQuestion(questions);
+//       // String SQL = "INSERT INTO public.\"questions \" (email,text) VALUES(?,?)";
+//        String SQL = "INSERT INTO `Questions` ( `Email`, `Text`) VALUES(?,?)";
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Connection dbConnection = DriverManager.getConnection(propertyServiceSecondDataBase.getUrl(), propertyServiceSecondDataBase.getUsername(), propertyServiceSecondDataBase.getPassword());
+//            PreparedStatement statement = dbConnection.prepareStatement(SQL);
+//            statement.setString(1, email);
+//            statement.setString(2, text);
+//            statement.addBatch();
+//            statement.executeBatch();
+//            System.out.println("123123");
+//        } catch (SQLException | ClassNotFoundException e) {
+//            logger.error(e.toString());
+//        }
 
         return new ModelAndView("contact");
     }
