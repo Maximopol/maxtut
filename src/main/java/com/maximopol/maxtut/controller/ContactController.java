4c9238befe69c1a13713d0bbe5f19cac22388d92
Maksim <maximopolnate@gmail.com>
2021-02-13 18:52:19 +0300

    Added MySQL

--- src/main/java/com/maximopol/maxtut/controller/ContactController.java
+++ src/main/java/com/maximopol/maxtut/controller/ContactController.java
@@ -43,14 +43,17 @@ public class ContactController {
     public ModelAndView sentQuestionToMyEmail(HttpServletRequest req, HttpServletResponse res, Model model, @ModelAttribute("user") User user) {
         String email = req.getParameterValues("email")[0], text = req.getParameterValues("message")[0];
 
-        String SQL = "INSERT INTO public.\"questions \" (email,text) VALUES(?,?)";
+
+       // String SQL = "INSERT INTO public.\"questions \" (email,text) VALUES(?,?)";
+        String SQL = "INSERT INTO `Questions` ( `Email`, `Text`) VALUES(?,?)";
         try (Connection dbConnection = DriverManager.getConnection(propertyServiceSecondDataBase.getUrl(), propertyServiceSecondDataBase.getUsername(), propertyServiceSecondDataBase.getPassword()); PreparedStatement statement = dbConnection.prepareStatement(SQL);) {
             statement.setString(1, email);
             statement.setString(2, text);
             statement.addBatch();
             statement.executeBatch();
+            System.out.println("123123");
         } catch (SQLException e) {
-            logger.error(e.getSQLState());
+            logger.error(e.toString());
         }
 
         return new ModelAndView("contact");
